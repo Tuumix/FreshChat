99fc09ed79b150774e3b64e9a79349eb808f6148
Tuumix <wellz-tkn@hotmail.com>
2020-03-29 18:42:21 -0300

    need to fix the error

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {Freshchat, FreshchatConfig} from 'react-native-freshchat-sdk';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+function App() {
+  /*var freshchatConfig = new FreshchatConfig(
+    'be3ea902-7b0a-41ab-a74c-6dd28c87ea64',
+    '5e4cc289-2c0c-49ab-b3ee-aabec3653e09',
+  );
+  Freshchat.init(freshchatConfig);*/
+  return (
+    <>
+      <Text>ddds</Text>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;

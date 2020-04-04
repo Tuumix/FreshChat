94d41cc6bc1a5bec569a46a9e935f8fc271047f7
Tuumix <wellz-tkn@hotmail.com>
2020-04-04 07:22:43 -0300

    Made a few changes

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React, {useState} from 'react';
 import {Freshchat, FreshchatConfig} from 'react-native-freshchat-sdk';
 import {FreshchatUser} from 'react-native-freshchat-sdk';
+//import {Button, ThemeProvider} from 'react-native-elements';
 
 import {
   SafeAreaView,
@@ -18,21 +19,23 @@ import {
   Text,
   Button,
   StatusBar,
+  TouchableOpacity,
 } from 'react-native';
 
 function App() {
   const [str, altera] = useState('teste');
-
   var freshchatConfig = new FreshchatConfig(
     'b6fd52a0-b34a-4455-8cb5-64273db96faa',
     '7f554b93-72db-4d49-8e68-59c50ba2abd6',
   );
+
   var freshchatUser = new FreshchatUser();
   freshchatUser.firstName = 'Wellington';
   freshchatUser.lastName = 'Doe';
   freshchatUser.email = 'tumi_cross@msn.com';
   freshchatUser.phoneCountryCode = '+55';
   freshchatUser.phone = '18996221772';
+
   Freshchat.setUser(freshchatUser, error => {
     console.log(error);
   });
@@ -40,19 +43,17 @@ function App() {
   Freshchat.identifyUser('amor@hotmail.com', null, error => {
     console.log(error);
   });
+
   Freshchat.addEventListener(Freshchat.EVENT_USER_RESTORE_ID_GENERATED, () => {
     console.log('onRestoreIdUpdated triggered');
     Freshchat.getUser(user => {
       var restoreId = user.restoreId;
       var externalId = user.externalId;
-      alert('gerado!');
     });
   });
 
   function abrir_chat() {
-    var restoreId;
     Freshchat.init(freshchatConfig);
-    //Freshchat.showFAQs();
     Freshchat.showConversations();
   }
 
@@ -65,7 +66,6 @@ function App() {
     Freshchat.getUser(user => {
       restoreId = user.restoreId;
       externalId = user.externalId;
-      alert('testando:' + restoreId);
     });
     Freshchat.identifyUser(externalId, restoreId, error => {
       console.log(error);
@@ -79,14 +79,13 @@ function App() {
 
   return (
     <View style={styles.Main}>
-      <View style={styles.Head}>
-        <Text>teste</Text>
-      </View>
-      <View style={styles.Footer}>
-        <Button onPress={abrir_FAQ} title="Email" />
-        <Button onPress={abrir_chat} title="Chat" />
-        <Button onPress={load} title="Load" />
-        <Button onPress={reset} title="Reset" />
+      <View style={styles.Botoes_Container}>
+        <TouchableOpacity style={styles.button} onPress={abrir_FAQ}>
+          <Text style={styles.text_button}>FAQ</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={abrir_chat}>
+          <Text style={styles.text_button}>Chat</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -95,21 +94,36 @@ function App() {
 const styles = StyleSheet.create({
   Main: {
     flex: 1,
-    backgroundColor: '#333',
-    flexDirection: 'column',
+    backgroundColor: '#f26641',
     width: '100%',
     height: 600,
   },
-  Head: {
-    backgroundColor: 'pink',
-    height: 650,
+  Botoes_Container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 30,
+    justifyContent: 'space-evenly',
+    paddingBottom: 30,
+    height: 100,
     width: '100%',
   },
-  Footer: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
+  button: {
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    width: '50%',
+    height: 50,
+    shadowColor: 'rgba(0,0,0, .4)', // IOS
+    shadowOffset: {height: 6, width: 3}, // IOS
+    shadowOpacity: 1, // IOS
+    shadowRadius: 1, //IOS
+  },
+  text_button: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
 
